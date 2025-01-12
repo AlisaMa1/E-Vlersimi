71dde8263ba533b7a7e50a9e7356ab7b1fed2510
AlisaMa1 <102972968+AlisaMa1@users.noreply.github.com>
2025-01-12 17:46:45 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using E_Vlersimiii.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<E_VleresimiiContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DBconnection"));
+});
+
+builder.Services.AddCors();
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Your API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
